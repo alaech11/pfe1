6ab70841c01f9b116283b84ec690fea01a3bc8a2
alaech11 <116915024+alaech11@users.noreply.github.com>
2022-11-03 21:57:58 +0100

    Update Projets.js

--- src/components/pages/Projets.js
+++ src/components/pages/Projets.js
@@ -1,39 +1,8 @@
-import React from "react";
+//import React from "react";
 import '../../App.css'
-import CardItem from "../CardItem";
-import '../Cards.css';
+
 
 export default function Projets(){
-    return (
+    return <index/>
     
-    <div className='cards__container'>
-    <div className='cards__wrapper'>
-      
-       
-      <ul className='cards__items'>
-        <CardItem
-          src='images/OIPe.jpg'
-          text='ælUUAALJFLJSDHFGUEALluhsjhgljhdflghlsrthg'
-          label='machin learning'
-          path='/desciption'
-        />
-        <CardItem
-          src='images/OIPg.jpg'
-          text='jhajhfjhjhfdghuhafuhguhasdfhjghjhdafgu'
-          label='games developement'
-          path='/desciption'
-        />
-        <CardItem
-          src='images/oom.jpeg'
-          text='luhaerlguhuhdfahuuherhguhjhdfnljadgljhd'
-          label='mobile app'
-          path='/desciption'
-        />
-
-      
-      </ul>
-    </div>
-</div>
-   ) 
-
-}
+    }
