a13a49c148b035d77e845c4b64f95923f16c9a53
alaech11 <116915024+alaech11@users.noreply.github.com>
2022-11-15 16:22:29 +0100

    Update Inscrivez.js

--- src/components/pages/Inscrivez.js
+++ src/components/pages/Inscrivez.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../../App.css'
-import Form from "../Form.js";
+/*import Form from "../Form.js";*/
 
 export default function Inscrivez(){
-    return
+    return 
 }
